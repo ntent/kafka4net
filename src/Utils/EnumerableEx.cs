33616834adfc85040c6d48a5bc148b3b1bbfc1b7
Vadim Chekan <kot.begemot@gmail.com>
2014-11-13 09:11:01 -0800

    Use DebuggerStepThrough attribute for uninteresting code

--- src/Utils/EnumerableEx.cs
+++ src/Utils/EnumerableEx.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace kafka4net.Utils
 {
     static class EnumerableEx
     {
+        [DebuggerStepThrough]
         public static void ForEach<T>(this IEnumerable<T> items, Action<T> action) 
         {
             foreach (T item in items)
