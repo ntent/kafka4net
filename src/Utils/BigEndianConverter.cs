3537cad6a0c18be325a403a6149c46807c79e7ca
Vadim Chekan <kot.begemot@gmail.com>
2014-11-13 11:20:45 -0800

    Apparently MemoryStream does not check boundaries and it is possible to read past end of stream.

--- src/Utils/BigEndianConverter.cs
+++ src/Utils/BigEndianConverter.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace kafka4net.Utils
 {
@@ -11,16 +7,23 @@ namespace kafka4net.Utils
     {
         public static int ReadInt32(MemoryStream s)
         {
+            if(s.Position + 4 > s.Length)
+                throw new Exception(string.Format("ReadInt32 needs 4 bytes but got ony {0}", s.Length - s.Position));
             return s.ReadByte() << 3 * 8 | s.ReadByte() << 2 * 8 | s.ReadByte() << 8 | s.ReadByte();
         }
 
         public static short ReadInt16(MemoryStream s)
         {
+            if (s.Position + 2 > s.Length)
+                throw new Exception(string.Format("ReadInt16 needs 2 bytes but got ony {0}", s.Length - s.Position));
             return (short)((s.ReadByte() << 8) | s.ReadByte());
         }
 
         public static long ReadInt64(MemoryStream stream)
         {
+            if (stream.Position + 8 > stream.Length)
+                throw new Exception(string.Format("ReadInt64 needs 8 bytes but got ony {0}", stream.Length - stream.Position));
+
             var res = 0L;
             for (int i = 0; i < 8; i++)
                 res = res << 8 | stream.ReadByte();
