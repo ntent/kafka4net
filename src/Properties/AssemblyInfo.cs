164ef5a3048dbdf2e144a3cc4272962395045acd
Vadim Chekan <kot.begemot@gmail.com>
2015-10-06 14:28:03 -0700

    Publish v-1.1
    Cleanup around nuget.

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -2,35 +2,15 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("kafka4net")]
 [assembly: AssemblyDescription("C# Kafka driver")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NTent")]
 [assembly: AssemblyProduct("kafka4net")]
 [assembly: AssemblyCopyright("Copyright © NTent 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b47c3611-9b2d-4124-9a87-4a094a5dc928")]
 
 [assembly: InternalsVisibleTo("kafka4net-tests")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyVersion("1.1.*")]
