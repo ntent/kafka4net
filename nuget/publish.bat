af0485fec6a835a5f0dc533f8385138237e4b99a
Vadim Chekan <kot.begemot@gmail.com>
2016-05-24 15:18:55 -0700

    Incomplete publish script
    Publish script needs integration with git task, for now version nneds to be set manually

--- nuget/publish.bat
+++ nuget/publish.bat
@@ -1,4 +1,4 @@
 @rem \bin\nuget-2.8.5\NuGet.exe pack ..\src\kafka4net.csproj -Build -Symbols -Prop Configuration=Release 
-nuget.exe pack ..\src\kafka4net.csproj -Build -Symbols -Prop Configuration=Release -Prop target=Clean -MSBuildVersion 14 -Verbosity detailed -Version 2.0.0-compression0001
+nuget.exe pack ..\src\kafka4net.csproj -Build -Symbols -Prop Configuration=Release; -MSBuildVersion 14 -Version 2.0.0-rc1
 
-@rem 
+@rem -Verbosity detailed
