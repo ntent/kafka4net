a2c96aa987ddf3aa73446b02bfc0c1635a1a09b2
thunderstumpges <tstumpges@ntent.com>
2017-03-15 15:27:05 -0700

    bump version to 2.0.1 and publish to nuget.org

--- nuget/publish.bat
+++ nuget/publish.bat
@@ -1,4 +1,4 @@
 @rem \bin\nuget-2.8.5\NuGet.exe pack ..\src\kafka4net.csproj -Build -Symbols -Prop Configuration=Release 
-nuget.exe pack ..\src\kafka4net.csproj -Build -Symbols -Prop Configuration=Release; -MSBuildVersion 14 -Version 2.0.0
+nuget.exe pack ..\src\kafka4net.csproj -Build -Symbols -Prop Configuration=Release; -MSBuildVersion 14 -Version 2.0.1
 
 @rem -Verbosity detailed
